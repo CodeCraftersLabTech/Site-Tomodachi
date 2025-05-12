d1feef77c7603bd08698cb829a7fdda9c9911054
Thaynafp <tflopes01@gmail.com>
2025-05-12 01:45:35 -0300

    fazendo carrosel pag home

---
+++ src/JS/home.js
@@ -0,0 +1,24 @@
+let indexAtual = 0;
+const slides = document.querySelectorAll('.slide');
+const legendas = document.querySelectorAll('.legenda');
+const totalSlides = slides.length;
+
+function mostrarSlide(index) {
+  // Atualiza slides
+  slides.forEach(slide => slide.classList.remove('ativo'));
+  slides[index].classList.add('ativo');
+
+  // Atualiza legendas
+  legendas.forEach(legenda => legenda.classList.remove('ativo'));
+  setTimeout(() => {
+    legendas[index].classList.add('ativo');
+  }, 1000); // Mostra a legenda com atraso
+}
+
+function proximoSlide() {
+  indexAtual = (indexAtual + 1) % totalSlides;
+  mostrarSlide(indexAtual);
+}
+
+mostrarSlide(indexAtual); // Inicial
+setInterval(proximoSlide, 4000); // Troca a cada 3 segundos
